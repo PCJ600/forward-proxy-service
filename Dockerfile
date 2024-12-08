bf122a40f4ca791e197772030c2011b72f63d71c
PC <nju_pc@163.com>
2024-12-08 01:25:32 -0500

    feat: some commits

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /
 RUN yum -y install procps net-tools squid
 COPY squid.conf /etc/squid/squid.conf
 COPY start_squid.sh /start_squid.sh
+
+RUN ulimit -n 65536
 RUN chmod +x start_squid.sh
 
 CMD ["sh", "-c", "/start_squid.sh"]
--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ IMAGE_TAG=$1
 [ -z "$IMAGE_TAG" ] && { echo "please input image tag"; exit 1; }
 mkdir -p output/
 
-#docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f Dockerfile .
-#docker save ${IMAGE_NAME}:${IMAGE_TAG} > output/${IMAGE_NAME}-${IMAGE_TAG}.tar
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f Dockerfile .
+docker save ${IMAGE_NAME}:${IMAGE_TAG} > output/${IMAGE_NAME}-${IMAGE_TAG}.tar
 
 cp -r deploy/ output/
 sed -i "s/IMAGE_PLACEHOLDER/${IMAGE_TAG}/g" output/deploy/squid-deployment.yaml
